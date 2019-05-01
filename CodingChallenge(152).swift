e18e8a5a948fd64597f535f551e3cac3838840c9
mcspeedy3 <43735701+mcspeedy3@users.noreply.github.com>
2019-05-01 09:22:58 -0700

    Update CodingChallenge(152).swift

--- CodingChallenge(152).swift
+++ CodingChallenge(152).swift
@@ -1,1 +1,16 @@
-
+import Foundation
+var numbers:[Int] = [1,2,3,4]
+var probability:[Double] = [0.1,0.5,0.2,0.2]
+func returnPercent(num:[Int], prob:[Double])->([String]){
+    if  prob.count != num.count {
+        return ["Make sure both arrays have the same amount of elements."]
+    }
+    var text:[String] = []
+    for i in num {
+        var percent = prob[i]*100
+        text.append("\(num[i]), \(percent)% of the time.")
+    }
+    return text
+}
+let ans:[String] = returnPercent(num: numbers, prob: probability)
+print(ans)
